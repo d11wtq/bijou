6916b031048066a6e3bf93d124c92ebac952a428
d11wtq <chris@w3style.co.uk>
2015-11-12 16:16:36 +1100

    Make CLI actually a CLI.

--- src/github.com/d11wtq/bijou/main.go
+++ src/github.com/d11wtq/bijou/main.go
@@ -4,16 +4,31 @@ import (
 	"fmt"
 	"github.com/d11wtq/bijou/core"
 	"github.com/d11wtq/bijou/runtime"
+	"io/ioutil"
+	"os"
 )
 
-func main() {
-	src := `())`
+// Interpreter exit codes
+const (
+	ExitOK  = 0
+	ExitErr = 255
+)
 
-	res, err := runtime.Run(src, core.RootEnv())
-
-	if err == nil {
-		fmt.Println(res)
-	} else {
-		fmt.Println("error:", err)
+// Terminate with exit code 255 on error
+func haltIf(err error) {
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(ExitErr)
 	}
+}
+
+// Command line entry point.
+// Usage: ./bin/bijou < source.bjx
+func main() {
+	src, err := ioutil.ReadAll(os.Stdin)
+	haltIf(err)
+	res, err := runtime.Run(string(src), core.RootEnv())
+	haltIf(err)
+	fmt.Fprintln(os.Stdout, res)
+	os.Exit(ExitOK)
 }
