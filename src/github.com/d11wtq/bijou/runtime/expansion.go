b7ba622f2a0e0f2676f0bc21eeeea0b9453c5070
d11wtq <chris@w3style.co.uk>
2015-11-20 23:55:27 +1100

    Fix bug with macro expansion ignoring tail calls.

--- src/github.com/d11wtq/bijou/runtime/expansion.go
+++ src/github.com/d11wtq/bijou/runtime/expansion.go
@@ -2,9 +2,15 @@ package runtime
 
 // Process the elements of a macro call form
 func EvalExpansion(env Env, mc Expandable, args Sequence) (Value, error) {
-	v, err := mc.Expand(env, args)
+	res, err := mc.Expand(env, args)
+	t, ok := res.(TailCall)
+	if ok == true {
+		res, err = t.Return()
+	}
+
 	if err != nil {
 		return nil, err
 	}
-	return v.Eval(env)
+
+	return res.Eval(env)
 }
