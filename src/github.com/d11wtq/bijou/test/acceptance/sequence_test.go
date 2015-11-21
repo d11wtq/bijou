4acf73e0cbba20648bc991331ac77eb1f5047a80
d11wtq <chris@w3style.co.uk>
2015-11-22 00:02:43 +1100

    Add tests on (put).

--- src/github.com/d11wtq/bijou/test/acceptance/sequence_test.go
+++ src/github.com/d11wtq/bijou/test/acceptance/sequence_test.go
@@ -27,6 +27,25 @@ func TestTail(t *testing.T) {
 	AssertRunError(t, "(tail '() '())")
 }
 
+func TestPut(t *testing.T) {
+	AssertRunEqual(
+		t, "(put '() 42)",
+		runtime.EmptyList.Append(runtime.Int(42)),
+	)
+	AssertRunEqual(
+		t, "(put '(42 7) 2)",
+		runtime.
+			EmptyList.
+			Append(runtime.Int(2)).
+			Append(runtime.Int(42)).
+			Append(runtime.Int(7)),
+	)
+	AssertRunError(t, "(put)")
+	AssertRunError(t, "(put '())")
+	AssertRunError(t, "(put 42)")
+	AssertRunError(t, "(put '() 42 7)")
+}
+
 func TestEmpty(t *testing.T) {
 	AssertRunEqual(t, "(empty? '())", runtime.True)
 	AssertRunEqual(t, "(empty? '(42 7 8))", runtime.False)
